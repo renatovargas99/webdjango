52ecf71434d38125a274e9cd8f06303fabdc3156
Gustavo Vega <gdavis.gv@gmail.com>
2023-04-13 21:32:33 -0400

    Subi Archivos

--- SoulG/urls.py
+++ SoulG/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import Aventura, index
 
 urlpatterns = [
     path('', index, name='index'),
-
+    path('', Aventura, name='Aventura')
 ]
--- SoulG/views.py
+++ SoulG/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-
     return render(request, 'SoulG/index.html')
+
+
+def Aventura(request):
+    return render(request, 'SoulG/Aventura.html')
+
