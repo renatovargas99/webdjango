48b0878ba260e13bb5a50b7517ede534b2758879
RaidonCL <114269570+RaidonCL@users.noreply.github.com>
2023-04-15 18:40:52 -0400

    Subida de paginas
    Se arreglan todas las paginas con sus respectivos urls

--- SoulG/urls.py
+++ SoulG/urls.py
@@ -1,7 +1,28 @@
 from django.urls import path
-from .views import Aventura, index
+from .views import (index, Aventura, Combate, Deporte , Rol , Terror , Registro , Inicio_Session , Carrito , AcM , Adinfinitum, Diablo, Easport, RE4, RecContrasena, 
+                   Session, SF6, Starfield, SWfw, WWE )
+
 
 urlpatterns = [
     path('', index, name='index'),
-    path('', Aventura, name='Aventura')
+    path('Aventura', Aventura, name='Aventura'),
+    path('Combate', Combate, name='Combate'),
+    path('Deporte', Deporte, name='Deporte'),
+    path('Rol', Rol, name='Rol'),
+    path('Terror', Terror, name='Terror'),
+    path('Registro', Registro, name='Registro'),
+    path('Inicio_Session', Inicio_Session, name='Inicio_Session'),
+    path('Carrito', Carrito, name='Carrito'),
+    path('AcM', AcM, name='AcM'),
+    path('Adinfinitum', Adinfinitum, name='Adinfinitum'),
+    path('Diablo', Diablo , name='Diablo'),
+    path('Easport', Easport, name='Easport'),
+    path('RE4', RE4, name='RE4'),
+    path('RecContrasena', RecContrasena, name='RecContrasena'),
+    path('Session', Session, name='Session'),
+    path('SF6', SF6, name='SF6'),
+    path('Starfield', Starfield, name='Starfield'),
+    path('SWfw', SWfw, name='SWfw'),
+    path('WWE', WWE, name='WWE'),
+
 ]
--- SoulG/views.py
+++ SoulG/views.py
@@ -4,7 +4,59 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'SoulG/index.html')
 
-
 def Aventura(request):
     return render(request, 'SoulG/Aventura.html')
 
+def Combate(request):
+    return render(request, 'SoulG/Combate.html')
+
+def Deporte(request):
+    return render(request, 'SoulG/Deporte.html')
+
+def Rol(request):
+    return render(request, 'SoulG/Rol.html')
+
+def Terror(request):
+    return render(request, 'SoulG/Terror.html')
+
+def Registro(request):
+    return render(request, 'SoulG/Registro.html')
+
+def Inicio_Session(request):
+    return render(request, 'SoulG/Inicio_Session.html')
+
+def Carrito(request):
+    return render(request, 'SoulG/Carrito.html')
+
+def Adinfinitum(request):
+    return render(request, 'SoulG/Ad_infinitum.html')
+
+def AcM(request):
+    return render(request, 'SoulG/AcM.html')
+
+def Diablo(request):
+    return render(request, 'SoulG/Diablo.html')
+
+def Easport(request):
+    return render(request, 'SoulG/Easport.html')
+
+def RE4(request):
+    return render(request, 'SoulG/RE4.html')
+
+def Session(request):
+    return render(request, 'SoulG/Session.html')
+
+def SF6(request):
+    return render(request, 'SoulG/SF6.html')
+
+def Starfield(request):
+    return render(request, 'SoulG/Starfield.html')
+
+def SWfw(request):
+    return render(request, 'SoulG/SWfw.html')
+
+def WWE(request):
+    return render(request, 'SoulG/WWE.html')
+
+def RecContrasena(request):
+    return render(request, 'SoulG/RecContrasena.html')
