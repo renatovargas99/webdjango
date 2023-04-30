ff1d67e2ceae9c883e874531873a173e647d9ba1
renatovargas99 <renato.vargas.molina@gmail.com>
2023-04-29 20:50:08 -0400

    Creacion de API REST y seguridad basada con token

---
+++ apirest/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import PostSerializer
+from .models import Post
+from rest_framework import status
+from django.http import Http404
+    
+class Post_APIView(APIView):
+    def get(self, request, format=None, *args, **kwargs):
+        post = Post.objects.all()
+        serializer = PostSerializer(post, many=True)
+        
+        return Response(serializer.data)
+    def post(self, request, format=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class Post_APIView_Detail(APIView):
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post)  
+        return Response(serializer.data)
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+# Create your views here.
